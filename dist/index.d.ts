5d5cb3657f592df291884c71ef60ffbb982e3262
g.usoltsev <g.usoltsev@eastwind.ru>
2025-07-11 17:47:27 +0500

    add

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-interface ButtonProps {
-    className?: string;
-}
-declare const Button: FC<ButtonProps>;
-
-export { Button };
-export type { ButtonProps };
--- dist/ui/Button/Button.d.ts
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react';
-export interface ButtonProps {
-    className?: string;
-}
-export declare const Button: FC<ButtonProps>;
