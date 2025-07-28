3538845bb82b7b68c58c795248c0f0f49c749744
g.usoltsev <g.usoltsev@eastwind.ru>
2025-07-28 15:04:10 +0500

    add

--- plopfile.cjs
+++ plopfile.cjs
@@ -1,8 +1,37 @@
 module.exports = function (plop) {
   plop.setHelper('eq', (a, b) => a === b);
 
+  plop.setGenerator('component-ui-type', {
+    description: 'Создания шаблона компонента в папку ui (styled, types, index, component, stories)',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Названия компонента (CamelCase):',
+        validate: (value) => {
+          if (!/^[A-Z][a-zA-Z0-9]*$/.test(value)) {
+            return 'Component name must be in CamelCase';
+          }
+          return true;
+        },
+      },
+    ],
+    data: {
+      name: '{{pascalCase name}}',
+    },
+    actions: [
+      {
+        type: 'addMany',
+        destination: 'src/ui/{{pascalCase name}}',
+        templateFiles: 'plop-templates/component-ui-type/*.hbs',
+        base: 'plop-templates/component-ui-type',
+        abortOnFail: true,
+      },
+    ],
+  });
+
   plop.setGenerator('component', {
-    description: 'Создания шаблона компонента в папку ui',
+    description: 'Создания шаблона компонента в папку ui (styled, index, component, stories)',
     prompts: [
       {
         type: 'input',
